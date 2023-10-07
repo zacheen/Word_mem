23e0c563e71bb145a788f8fa3f7c52f255411a05
zacheen <zacheenoao@gmail.com>
2023-10-07 20:53:05 +0800

    為了不要變更到看過的順序，改成直接隨機

--- Main.py
+++ Main.py
@@ -11,25 +11,34 @@ class Words :
         fr = open(SETT.word_file_path, "r")
         self.old_word_list = json.loads(fr.read())         # 沒有超過日期的單字
         print("總共讀取單字數量 : ", len(self.old_word_list))
-        self.time_past_list = []  # 不能用 deque 因為要 pop  # 超過日期的單字
         self.new_word_list = []
-        indx = 0
-        for _ in range(len(self.old_word_list)):
+        # 找 random 起使位置
+        for indx in range(len(self.old_word_list)):
             word_last_date = self.old_word_list[indx]["date"]
             word_last_date = datetime.strptime(word_last_date, SETT.DATE_FORMAT)
             if word_last_date < self.now_time :
-                self.time_past_list.append(self.old_word_list.pop(indx))
-            else :
-                indx += 1
+                self.start_indx = indx
+                break
+        self.NEAR_FIRST = 50 # 最近看過的項目優先隨機到
+        self.NEAR_FIRST += self.start_indx
+        self.rand_before = set()
 
     # 方法1 : 隨機直到日期以內
     # 方法2 : 先挑出日期以內再隨機 (但是我希望可以不要弄亂順序)
     def random_within_date(self):
-        NEAR_FIRST = 100 # 最近看過的項目優先隨機到
-        rand_range = len(self.time_past_list)
-        if NEAR_FIRST > 0 :
-            rand_range = min(rand_range, NEAR_FIRST)
-        return self.time_past_list.pop(randrange(rand_range))
+        rand_range = len(self.old_word_list)
+        if self.NEAR_FIRST > 0 :
+            rand_range = min(rand_range, self.NEAR_FIRST)
+        rand_indx = randrange(self.start_indx, rand_range)
+        for indx in range(rand_indx, len(self.old_word_list)) :
+            word_last_date = self.old_word_list[indx]["date"]
+            word_last_date = datetime.strptime(word_last_date, SETT.DATE_FORMAT)
+            if word_last_date < self.now_time :
+                return self.old_word_list.pop(indx)
+            else :
+                if self.old_word_list[indx]["eng"] not in self.rand_before :
+                    self.NEAR_FIRST += 1
+                    self.rand_before.add(self.old_word_list[indx]["eng"])
     
     def add_word(self, item):
         self.new_word_list.insert(0,item) # ?? 有可能會花很久
@@ -39,7 +48,7 @@ class Words :
             random_word = []
         else :
             random_word = [random_word]
-        all_words = self.new_word_list + random_word + self.time_past_list + self.old_word_list
+        all_words = self.new_word_list + random_word + self.old_word_list
         print("總共寫入單字數量 : ", len(all_words))
         with open(SETT.word_file_path, "w", encoding='UTF-8') as fw:
             json.dump(all_words, fw, indent = 4, ensure_ascii=False)
@@ -79,7 +88,6 @@ if __name__ == "__main__" :
         show_str = rand_word["eng"]
         show_txt.config(text = show_str )
         word_to_sound(show_str)
-        print(rand_word)
 
     # 按鈕初始化
     button_show_ans = tk.Button(window,text = '顯示翻譯',font = ('黑體', 15))
@@ -99,21 +107,24 @@ if __name__ == "__main__" :
             button_status = 0
         else :
             # change to know
-            show_str = show_txt.cget("text") + "\n" +rand_word["chi"]
+            show_str = show_txt.cget("text") + " " +rand_word["chi"]
             if rand_word["association"] != "no" :
                 show_str += "\n" +rand_word["association"]
+            if "other" in rand_word :
+                for other_type in rand_word["other"] :
+                    show_str += "\n" + other_type
             show_txt.config(text=show_str)
             button_test_pass.place(relx=0,rely=word_show_weight,relheight=1-word_show_weight,relwidth=0.5)
             button_test_fail.place(relx=0.5,rely=word_show_weight,relheight=1-word_show_weight,relwidth=0.5)
             button_show_ans.place_forget()
             button_status = 1
-        print("switch finish now button_status : ",button_status)
+        # print("switch finish now button_status :",button_status)
     switch_button()
     
     # 按鈕 function
     def test_pass(word):
-        print(word["status"])
-        print(SETT.DAYS[word["status"]])
+        # print(word["status"])
+        # print(SETT.DAYS[word["status"]])
         word["date"] = (datetime.now() + timedelta(days=SETT.DAYS[word["status"]])).strftime(SETT.DATE_FORMAT)
         word["status"] = min(word["status"]+1, len(SETT.DAYS)-1)
         words.add_word(rand_word)
